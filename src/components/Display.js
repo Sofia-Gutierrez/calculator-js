cd2c73bdbe8dd6d6d45837d16b6d14e79054c5b2
Sofia_Gutierrez <sofiagutierrez1845@gmail.com>
2023-05-05 15:55:27 -0300

    cambiando detalle en el input

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../assets/css/Input.css";
 
 const Display = ({ input, limit }) => (
-        <input value={!limit ? input : limit} maxLength="12" className="input"/>
+        <input value={!limit ? input : limit} maxLength="12" className="input" placeholder="0"/>
 );
 
 export default Display;
